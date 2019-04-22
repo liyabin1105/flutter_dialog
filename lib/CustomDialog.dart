ea12cfcb8ca4a271228d8fe71fcb2aca938704bf
liyabin <liyabin@xiaohulu.com>
2019-04-22 16:37:26 +0800

    outsideDismiss为true时手机返回键不能关闭弹窗

--- lib/CustomDialog.dart
+++ lib/CustomDialog.dart
@@ -42,98 +42,103 @@ class _CustomDialogState extends State<CustomDialog> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final width = size.width;
-    return GestureDetector(
-      onTap: () => {
+    return WillPopScope(
+      child: GestureDetector(
+        onTap: () => {
         widget.outsideDismiss ? _dismissDialog() : null
-      },
-      child: Material(
-        type: MaterialType.transparency,
-        child: Center(
-          child: Container(
-            width: width - 100.0,
-            height: 150.0,
-            alignment: Alignment.center,
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: widget.title == null ? 0 : 16.0
-                ),
-                Text(
-                  widget.title == null ? '' : widget.title,
-                  style: TextStyle(fontSize: 16.0)
-                ),
-                Expanded(
-                  child: Center(
-                    child: Text(
-                      widget.content == null ? '' : widget.content,
-                      style: TextStyle(fontSize: 14.0)
-                    ),
+        },
+        child: Material(
+          type: MaterialType.transparency,
+          child: Center(
+            child: Container(
+              width: width - 100.0,
+              height: 150.0,
+              alignment: Alignment.center,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                      height: widget.title == null ? 0 : 16.0
                   ),
-                  flex: 1
-                ),
-                SizedBox(
-                  height: 1.0,
-                  child: Container(color: Color(0xDBDBDBDB))
-                ),
-                Container(
-                  height: 45,
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: widget.isCancel ? Container(
-                          decoration: BoxDecoration(
-                            color: widget.cancelColor == null ? Color(0xFFFFFFFF) : widget.cancelColor,
-                            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(12.0))
-                          ),
-                          child: FlatButton(
-                            child: Text(
-                              '取消',
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                color: widget.cancelColor == null ? Colors.black87 : Color(0xFFFFFFFF),
-                              )
-                            ),
-                            onPressed: _dismissDialog,
-                            splashColor: widget.cancelColor == null ? Color(0xFFFFFFFF) : widget.cancelColor,
-                            highlightColor: widget.cancelColor == null ? Color(0xFFFFFFFF) : widget.cancelColor,
-                          ),
-                        ) : Text(''),
-                        flex: widget.isCancel ? 1 : 0
-                      ),
-                      SizedBox(width: widget.isCancel ? 1.0 : 0,child: Container(color: Color(0xDBDBDBDB))),
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: widget.confirmColor == null ? Color(0xFFFFFFFF) : widget.confirmColor,
-                              borderRadius: widget.isCancel ? BorderRadius.only(bottomRight: Radius.circular(12.0)) : BorderRadius.only(bottomLeft: Radius.circular(12.0), bottomRight: Radius.circular(12.0))
-                          ),
-                          child: FlatButton(
-                            onPressed: _confirmDialog,
-                            child: Text(
-                              widget.confirmContent == null ? '确定' : widget.confirmContent,
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                color: widget.confirmColor == null ? (widget.confirmTextColor == null ? Colors.black87 : widget.confirmTextColor) : Color(0xFFFFFFFF),
-                              )
-                            ),
-                            splashColor: widget.confirmColor == null ? Color(0xFFFFFFFF) : widget.confirmColor,
-                            highlightColor: widget.confirmColor == null ? Color(0xFFFFFFFF) : widget.confirmColor,
-                          ),
+                  Text(
+                      widget.title == null ? '' : widget.title,
+                      style: TextStyle(fontSize: 16.0)
+                  ),
+                  Expanded(
+                      child: Center(
+                        child: Text(
+                            widget.content == null ? '' : widget.content,
+                            style: TextStyle(fontSize: 14.0)
                         ),
-                        flex: 1
                       ),
-                    ],
+                      flex: 1
+                  ),
+                  SizedBox(
+                      height: 1.0,
+                      child: Container(color: Color(0xDBDBDBDB))
+                  ),
+                  Container(
+                      height: 45,
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                              child: widget.isCancel ? Container(
+                                decoration: BoxDecoration(
+                                    color: widget.cancelColor == null ? Color(0xFFFFFFFF) : widget.cancelColor,
+                                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(12.0))
+                                ),
+                                child: FlatButton(
+                                  child: Text(
+                                      '取消',
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        color: widget.cancelColor == null ? Colors.black87 : Color(0xFFFFFFFF),
+                                      )
+                                  ),
+                                  onPressed: _dismissDialog,
+                                  splashColor: widget.cancelColor == null ? Color(0xFFFFFFFF) : widget.cancelColor,
+                                  highlightColor: widget.cancelColor == null ? Color(0xFFFFFFFF) : widget.cancelColor,
+                                ),
+                              ) : Text(''),
+                              flex: widget.isCancel ? 1 : 0
+                          ),
+                          SizedBox(width: widget.isCancel ? 1.0 : 0,child: Container(color: Color(0xDBDBDBDB))),
+                          Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: widget.confirmColor == null ? Color(0xFFFFFFFF) : widget.confirmColor,
+                                    borderRadius: widget.isCancel ? BorderRadius.only(bottomRight: Radius.circular(12.0)) : BorderRadius.only(bottomLeft: Radius.circular(12.0), bottomRight: Radius.circular(12.0))
+                                ),
+                                child: FlatButton(
+                                  onPressed: _confirmDialog,
+                                  child: Text(
+                                      widget.confirmContent == null ? '确定' : widget.confirmContent,
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        color: widget.confirmColor == null ? (widget.confirmTextColor == null ? Colors.black87 : widget.confirmTextColor) : Color(0xFFFFFFFF),
+                                      )
+                                  ),
+                                  splashColor: widget.confirmColor == null ? Color(0xFFFFFFFF) : widget.confirmColor,
+                                  highlightColor: widget.confirmColor == null ? Color(0xFFFFFFFF) : widget.confirmColor,
+                                ),
+                              ),
+                              flex: 1
+                          ),
+                        ],
+                      )
                   )
-                )
-              ],
-            ),
-            decoration: BoxDecoration(
-              color: Color(0xFFFFFFFF),
-              borderRadius: BorderRadius.circular(12.0)
+                ],
+              ),
+              decoration: BoxDecoration(
+                  color: Color(0xFFFFFFFF),
+                  borderRadius: BorderRadius.circular(12.0)
+              ),
             ),
           ),
         ),
       ),
+      onWillPop: () async {
+        return widget.outsideDismiss;
+      }
     );
   }
 }
